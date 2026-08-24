89160a0bebf0cbc6b4d16b1f41a6a6d2c1148885
Ratanmani <ratanmanisingh553@gmail.com>
2026-08-24 14:38:50 +0530

    Function overloading

--- Practice Problems/function_overloading.cpp
+++ Practice Problems/function_overloading.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int multiply(int a, int b){
-    return a*b;
-}
-int multiply(int a, int b,int c)
+// 1. Overloading by changing the count of parameters;
+// int multiply(int a, int b){
+//     return a*b;
+// }
+// int multiply(int a, int b,int c)
+// {
+//     return a * b *c;
+// }
+// int main(){
+//     cout << multiply(10,20)<<endl;
+//     cout << multiply(10,20,3)<<endl;
+// }
+
+// 2. Overloading by changing parameter Types
+int multiply(int a, int b)
 {
-    return a * b *c;
+    return a * b;
 }
-int main(){
-    cout << multiply(10,20)<<endl;
-    cout << multiply(10,20,3)<<endl;
+double multiply(double a, double b)
+{
+    return a * b ;
+}
+int main()
+{
+    cout << multiply(10, 20) << endl;
+    cout << multiply(10.3,5.6) << endl;
 }
