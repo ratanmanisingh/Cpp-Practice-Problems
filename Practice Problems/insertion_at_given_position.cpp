2684f2b3d5cd51aa09920a1b5e76410d8be26d5b
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-09 16:23:52 +0530

    Insertion at a given Position

---
+++ Practice Problems/insertion_at_given_position.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of element you want to insert: ";
+    cin >> n;
+    int arr[50];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter the number: ";
+        cin >> arr[i];
+    }
+    int position;
+    int element;
+    cout << "Enter the Index number where you want to insert: ";
+    cin >> position;
+    cout << "Enter the elements you want to insert: ";
+    cin >> element;
+    for (int i = n; i >= position - 1; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+    arr[position] = element;
+    cout<<"After the insertion: ";
+    for (int i = 0; i < n + 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
