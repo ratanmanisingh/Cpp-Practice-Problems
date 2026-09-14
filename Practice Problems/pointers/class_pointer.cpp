a35a636ed08adca8bc86572e2aff3d12673a4d4b
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-14 14:32:20 +0530

    Class Pointer

--- Practice Problems/pointers/class_pointer.cpp
+++ Practice Problems/pointers/class_pointer.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 using namespace std;
 
-class pointer{
+// class pointer{
+//     int s;
+//     int *p=&s;
+//     public:
+//         void get_data(){
+//             cin>>s;
+//         }
+//         void display(){
+//             cout<<s<<endl;
+//             cout<<&s<<endl;
+//             cout<<p;
+//         }
+// };
+// int main(){
+//     pointer p;
+//     p.get_data();
+//     p.display();
+// }
+
+// Method 2:
+
+class array{
     int s;
-    int *p=&s;
     public:
-        void get_data(){
+        void getdata(){
+            cout<<"Enter value of s= ";
             cin>>s;
         }
-        void display(){
-            cout<<s<<endl;
-            cout<<&s<<endl;
-            cout<<p;
+        void add(){
+            cout<<"s value= "<<s;
+            cout<<"\nAddress of s= "<<&s;
         }
 };
-int main(){
-    pointer p;
-    p.get_data();
-    p.display();
-}
 
+int main(){
+    array a;
+    array *p=&a;
+    // p->getdata();
+    // p->add();
+    (*p).getdata();
+    (*p).add();
+    return 0;
+}
